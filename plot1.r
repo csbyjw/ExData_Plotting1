70662dffd580ad59f8f508cd5a57db7bfb1a8cae
csbyjw <csbyjw@gmail.com>
2014-06-08 16:25:49 -0500

    minor edit
    Minor edit

--- plot1.r
+++ plot1.r
@@ -18,6 +18,7 @@ for( i in 1:2) {
   inputdata[, i] <- as.character(inputdata[, i])
 }
 
+
 with(inputdata, hist(Global_active_power, xlab="Global Active Power (kilowatts)", col="red", main="Global Active Power"))
 dev.copy(png,file="plot1.png")
 dev.off()
